78226c6ed80aa9e48800135fe7f47c7e07231af2
alexandre.bruxelle <alexandre.bruxelle@gmail.com>
2019-12-04 22:56:08 +0100

    Jason is working

--- lib/larson_web/channels/larson_web/rabbit.ex
+++ lib/larson_web/channels/larson_web/rabbit.ex
@@ -5,7 +5,7 @@ defmodule LarsonWeb.Rabbit do
       receive do
         {:basic_deliver, payload, %{correlation_id: ^correlation_id}} ->
           payload
-          IO.puts "Rabbit. got : #{payload}"
+          IO.puts "Rabbit. got #{payload}"
       end
     end
 
@@ -17,13 +17,25 @@ defmodule LarsonWeb.Rabbit do
 
       IO.puts "Rabbit. declare "
 
-      {:ok, %{queue: queue_name}} = AMQP.Queue.declare(channel, "", exclusive: true)
+      {:ok, %{queue: queue_name}} = AMQP.Queue.declare( channel,
+                                                        "",
+                                                        exclusive: true)
       IO.puts "Rabbit. consume "
       AMQP.Basic.consume(channel, queue_name, nil, no_ack: true)
-      correlation_id = :erlang.unique_integer |> :erlang.integer_to_binary |> Base.encode64
-      request = message
+      correlation_id =
+        :erlang.unique_integer
+        |> :erlang.integer_to_binary
+        |> Base.encode64
+
+      #request = message
       IO.puts "Rabbit. publish #{correlation_id}"
-      AMQP.Basic.publish(channel, "", "rpc_queue", request, reply_to: queue_name, correlation_id: correlation_id)
+
+      AMQP.Basic.publish(channel,
+                          "",
+                          "rpc_queue",
+                          message,
+                          reply_to: queue_name,
+                          correlation_id: correlation_id)
 
       LarsonWeb.Rabbit.wait_for_messages(channel, correlation_id)
       IO.puts "Rabbit. end  "
--- lib/larson_web/channels/score_channel.ex
+++ lib/larson_web/channels/score_channel.ex
@@ -2,18 +2,26 @@ defmodule LarsonWeb.ScoreChannel do
   use LarsonWeb, :channel
 
   def join("score:platformer", _payload, socket) do
+    IO.puts "join"
     {:ok, socket}
   end
 
   def handle_in("broadcast_score", payload, socket) do
 
-    broadcast(socket, "broadcast_score", payload)
 
-    %{"player_score"=>%{"cmd"=>cmd, "fsx"=>fsx, "params"=>params}}=payload
+    #broadcast(socket, "broadcast_score", payload)
+    #{list} =
+    #%{"player_score"=>%{"cmd"=>cmd, "fsx"=>fsx, "params"=>params}}=payload
 
-    IO.puts " [x] Requesting (#{cmd} , #{fsx} , #{params})"
-    response = LarsonWeb.Rabbit.call(payload)
-    IO.puts " [.] Got #{response}"
+    #IO.puts " [x] Requesting (#{Jason.decode!(payload)})"
+    IO.puts "handle [x] Requesting"
+
+    mystr = Jason.encode!(payload)
+    IO.puts " [x] Requesting (#{mystr})"
+
+    #IO.puts " [x] Requesting (#{Jason.encode!(%{"cmd" => "build", "fsx" => "build.fsx", "params" => " "})}) "
+    response = LarsonWeb.Rabbit.call(mystr)
+    IO.puts "handle [.] Got #{response}"
 
     {:noreply, socket}
   end
